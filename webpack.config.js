db03b83fc0546c067cfe44f916b64c5424835fd0
Aakash Shah <ashah@lowes.com>
2020-01-01 14:45:13 +0530

    - setup: - html and clean webpack plugins:

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+  mode: 'development',
   entry: {
     foo: './src/index.js'
   },
   output: {
+    path: path.resolve('dist'),
     filename: '[name].[chunkhash:8].js'
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin()
+  ]
 }
